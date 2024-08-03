728d96ca506835145607557d573cf3f9d9df16e3
Dimas Purwanto <dimas@jalintraksi.com>
2024-08-03 22:49:04 +0700

    failed pipeline

--- src/components/home/Quotes.tsx
+++ src/components/home/Quotes.tsx
@@ -3,7 +3,7 @@ export const Quotes: React.FC = () => {
     return (
         <div className="px-6 py-12 md:px-24 md:py-28 text-center bg-gray-100 mt-14 text-black">
             <h1 className="text-2xl md:text-4xl">
-                "A villa is a personal retreat where luxury meets tranquility. With spacious rooms, elegant interiors, and beautiful gardens, it offers a perfect blend of comfort and serenity. Every aspect is designed to create a serene escape from the everyday hustle, allowing you to relax and enjoy life’s simple pleasures in style."
+            &quot;A villa is a personal retreat where luxury meets tranquility. With spacious rooms, elegant interiors, and beautiful gardens, it offers a perfect blend of comfort and serenity. Every aspect is designed to create a serene escape from the everyday hustle, allowing you to relax and enjoy life’s simple pleasures in style.&quot;
             </h1>
         </div>
     )
